cd308a1f644283a725515606b2aac25f937e3f44
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-12 16:36:42 +0530

    project running on Daphne ASGI server

--- webSockets/asgi.py
+++ webSockets/asgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter
+# Routes incoming connections based on the protocol type (HTTP or Web Socket)
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webSockets.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    # get_asgi_application creates connection between web server and django project
+    #If the incoming connection is an HTTP request, send it to Django's normal request handling system
+    
+    'http': get_asgi_application()
+})
--- webSockets/settings.py
+++ webSockets/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'channels',
     
     'django.contrib.admin',
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chat',
 ]
+
+ASGI_APPLICATION = 'webSockets.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
